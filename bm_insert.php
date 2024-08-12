ad14b278a0d73254911097775dc14880ab2a424a
makitonishizawa <168268389+makitonishizawa@users.noreply.github.com>
2024-08-12 23:52:57 +0900

    Update bm_insert.php

--- bm_insert.php
+++ bm_insert.php
@@ -21,7 +21,7 @@ if (isset($_FILES['image'])) {
 }
 
 // Google Maps APIを使用して位置情報を取得
-$apiKey = 'AIzaSyDY1AbFC0KarZrysvfWBknwJcFDx5dTXnI'; // 実際のAPIキーをここに設定
+$apiKey = ''; // 実際のAPIキーをここに設定
 $location = getLatLng($country, $apiKey);
 
 if ($location) {
